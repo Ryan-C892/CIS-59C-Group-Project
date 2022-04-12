9ab68b609d47ff26353ff16261c8572bfc983b05
Ryan Hetherington <hetheringtonr2@comcast.net>
2022-04-12 15:57:02 -0700

    added JS for blog

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 // Responsive Navbar //
-var navBtn = document.getElementById("navBtn");
-var navList = document.getElementById("navMenu")
+var navBtn = document.querySelector("#navBtn");
+var navList = document.querySelector("#navMenu")
 navBtn.addEventListener("click", ()=> {
     navList.classList.toggle("showNav");
+});
+
+// Load More //
+var loadMore = document.querySelector("#loadMore");
+var content = document.querySelectorAll(".content");
+var newArray = Array.from(content).slice(0, 4);
+newArray.forEach(element => {
+    element.classList.remove("none");
+});
+loadMore.addEventListener('click', ()=> {
+    var moreContent = Array.from(content).slice(5, 10);
+    moreContent.forEach(element => {
+        element.classList.toggle("none");
+    });
+    if (loadMore.innerHTML = "Less") {
+        loadMore.innerHMTL = "More";
+    } else {
+        loadMore.innerHTML = "Less";
+    }
 });
