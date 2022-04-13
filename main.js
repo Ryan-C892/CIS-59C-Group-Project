fdbf6d2375b322454a5e7c8067bc72fea623c248
Ryan Hetherington <hetheringtonr2@comcast.net>
2022-04-13 16:50:15 -0700

    added blog JS

--- main.js
+++ main.js
@@ -5,21 +5,26 @@ navBtn.addEventListener("click", ()=> {
     navList.classList.toggle("showNav");
 });
 
-// Load More //
-var loadMore = document.querySelector("#loadMore");
-var content = document.querySelectorAll(".content");
-var newArray = Array.from(content).slice(0, 4);
-newArray.forEach(element => {
-    element.classList.remove("none");
-});
-loadMore.addEventListener('click', ()=> {
-    var moreContent = Array.from(content).slice(5, 10);
-    moreContent.forEach(element => {
-        element.classList.toggle("none");
+// Events //
+function EventHandler() {
+    var loadMore = document.querySelector("#loadMore");
+    var content = document.querySelectorAll(".content");
+    var newArray = Array.from(content).slice(0, 4);
+    newArray.forEach(element => {
+        element.classList.remove("none");
     });
-    if (loadMore.innerHTML = "Less") {
-        loadMore.innerHMTL = "More";
-    } else {
-        loadMore.innerHTML = "Less";
-    }
-});
+    loadMore.addEventListener('click', ()=> {
+        var moreContent = Array.from(content).slice(5, 10);
+        moreContent.forEach(element => {
+            element.classList.toggle("none");
+        });
+    });
+    loadMore.addEventListener('click', ()=> {
+        if(loadMore.innerHTML == "Less") {
+            loadMore.innerHTML = "More";
+        } else {
+            loadMore.innerHTML = "Less";
+        }
+    });
+}
+EventHandler();
